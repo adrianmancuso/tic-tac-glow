5cbff2c877076cbaa76c0a3105795ccb0402f395
Adrian <adrian.mancuso@gmail.com>
2017-06-14 11:04:22 +1000

    added player turns and properties in js

--- tictactoe.js
+++ tictactoe.js
@@ -1,31 +1,64 @@
 var board = document.getElementsByTagName('main')[0];
-var playerOneName = document.getElementById('nameP1');
-var playerTwoName = document.getElementById('nameP2');
-var startingPlayer = "";
-var secondaryPlayer = "";
+var inputOne = document.getElementById('nameP1');
+var buttonP1 = document.getElementById('buttonP1');
+var inputTwo = document.getElementById('nameP2');
+var buttonP2 = document.getElementById('buttonP2');
+var currentPlayer = "";
+
+
+var playerOne = {
+	color: '#B26D91'
+};
+
+var playerTwo = {
+	color: '#14B8CC'
+};
+
+//player set up
+var playerOneProperties = function() {
+	playerOne.name = inputOne.value;
+	inputOne.value = "";
+	if (playerTwo.name !== undefined) {
+		chooseStartingPlayer();
+	}
+}
+
+var playerTwoProperties = function() {
+	playerTwo.name = inputTwo.value;
+	inputTwo.value = "";
+	if (playerOne.name !== undefined) {
+		chooseStartingPlayer();
+	}
+}
+
+buttonP1.addEventListener('click', playerOneProperties);
+buttonP2.addEventListener('click', playerTwoProperties);
+
+
 
 var chooseStartingPlayer = function (){
 	var x = Math.random();
 	if (x >= 0.5) {
-		startingPlayer = (playerOneName.value);
-		secondaryPlayer = (playerTwoName.value);
+		currentPlayer = (playerOne);
 	} else {
-		startingPlayer = (playerTwoName.value);
-		secondaryPlayer = (playerOneName.value);
+		currentPlayer = (playerTwo);
 	}
 }
 
-var changeColorX = function (event) {
-	if (event.target.style.backgroundColor = 'slategrey') {
-	event.target.style.backgroundColor = '#B26D91';
-	event.target.classList.add('glowing');
-	//code to change turn B26D91 B2698B
-}
+var takeTurn = function (event, colorInPlay) {
+	if (event.target.tagName === 'DIV' && event.target.style.backgroundColor === '') {
+		event.target.style.backgroundColor = colorInPlay;
+		event.target.classList.add('glowing');
+	}
+		if (currentPlayer === playerOne) {
+			currentPlayer = playerTwo;
+		} else {
+			currentPlayer = playerOne;
+		}
 }
 
-var changeColorY = function (event) {
-	event.target.style.backgroundColor = '#14B8CC';
-	event.target.classList.add('glowing');
+var endGameCheck = function() {
+
 }
 
-board.addEventListener('click', changeColorY);
+board.addEventListener('click', function(){takeTurn(event, currentPlayer.color)});
